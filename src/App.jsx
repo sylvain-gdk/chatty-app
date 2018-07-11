0366ad59449a0b259c35600dc7923d38a7669420
Sylvain Goedike <sgoedike@videotron.ca>
2018-07-11 22:33:32 +0000

    Added broadcasting features

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,6 @@ import React, {Component} from 'react';
 import ChatBar from "./ChatBar.jsx";
 import MessageList from "./MessageList.jsx";
 
-// Generates a random id
-const generateRandomId = (alphabet => {
-  const alphabetLength = alphabet.length;
-  const randoIter = (key, n) => {
-    if (n === 0) {
-      return key;
-    }
-    const randoIndex = Math.floor(Math.random() * alphabetLength);
-    const randoLetter = alphabet[randoIndex];
-    return randoIter(key + randoLetter, n - 1);
-  };
-  return () => randoIter("", 10);
-})("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
-
 // Main app component
 class App extends Component {
   constructor(){
@@ -31,15 +17,20 @@ class App extends Component {
   }
 
   componentDidMount() {
-    setTimeout(() => {
-      // Add a new message to the list of messages in the data store
-      const newMessage = {id: generateRandomId(), username: "Michelle", content: "Hello there!", type: 'incomingMessage'};
-      const messages = this.state.allMessages.messages.concat(newMessage);
-      // Update the state of the app component.
-      // Calling setState will trigger a call to render() in App and all child components.
-      this.setState({allMessages: {messages: messages}})
-    }, 3000)
-  }
+    this.socket = new WebSocket("ws://localhost:3001/websocket");
+    this.socket.addEventListener("open", e => {
+      console.log("Connected to websocket server");
+    });
+    // Updates messages
+    this.socket.onmessage = event => {
+      const message = JSON.parse(event.data);
+      const messages = this.state.allMessages.messages.concat(message)
+      this.setState({
+        currentUser: message.username,
+        allMessages: {messages: messages}
+      })
+    }
+  };
 
   // Adds a new message to an existing array
   addMessage = (userInput, content) => {
@@ -59,16 +50,11 @@ class App extends Component {
       const messages = this.state.allMessages.messages.concat(newNotification);
     }
     const newMessage = {
-      id: generateRandomId(),
       username: currentUser,
       content: content,
       type: 'incomingMessage'
     };
-    const messages = this.state.allMessages.messages.concat(newMessage)
-    this.setState({
-      currentUser: currentUser,
-      allMessages: {messages: messages}
-    })
+    this.socket.send(JSON.stringify(newMessage));
   }
 
   // Renders a list of messages from an array,
